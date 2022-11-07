bca98e2b9839da377e7fbd77688791f7ef9c1780
AncaKowa <110481563+AncaKowa@users.noreply.github.com>
2022-11-07 22:09:33 +0200

    Monochrome_tema_

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,16 @@
 export default function Home() {
   return (
     <div>
-      hello boston <i className="fa-solid fa-house"></i>
-      <img src="/images/logo.jpg" alt="Alt"></img>
+      Hello Monochrome! <i className="fa-solid fa-house"></i>
+      <img src="/images/banners/banner.png" alt="Alt"></img>
       <div>
         <button className="button button-primary">Button Primary</button>
         <button className="button button-primary button-large">
           Button Primary Large
         </button>
         <a className="button button-primary">Button Primary</a>
-        <a className="button button-primary button-large">
-          Button Primary Large
+        <a className="button button-primary button-inverted">
+          Button Primary Inverted
         </a>
       </div>
     </div>
